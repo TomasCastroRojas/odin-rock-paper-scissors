37b0328ad1001be6f6665afa2aff8a3306b63deb
TomasCastroRojas <tomascastrorojas18@gmail.com>
2023-07-27 11:46:40 -0300

    Change code structure, first global declarations then functions

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+const scoreInfo = document.getElementById('scoreInfo')
+const scoreMessage = document.getElementById('scoreMessage')
+const playerScorePara = document.getElementById('playerScore')
+const computerScorePara = document.getElementById('computerScore')
+const playerSign = document.getElementById('playerSign')
+const computerSign = document.getElementById('computerSign')
+const rockBtn = document.getElementById('rockBtn')
+const paperBtn = document.getElementById('paperBtn')
+const scissorsBtn = document.getElementById('scissorsBtn')
+const endgameModal = document.getElementById('endgameModal')
+const endgameMsg = document.getElementById('endgameMsg')
+const overlay = document.getElementById('overlay')
+const restartBtn = document.getElementById('restartBtn')
 
 let playerScore = 0;
 let computerScore = 0;
 let roundWinner = '';
+
+rockBtn.addEventListener('click', () => handleClick('ROCK'))
+paperBtn.addEventListener('click', () => handleClick('PAPER'))
+scissorsBtn.addEventListener('click', () => handleClick('SCISSORS'))
+restartBtn.addEventListener('click', restartGame)
+overlay.addEventListener('click', closeEndgameModal)
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
@@ -41,26 +60,6 @@ function getComputerChoice() {
 function isGameOver() {
   return playerScore === 5 || computerScore === 5;
 }
-
-const scoreInfo = document.getElementById('scoreInfo')
-const scoreMessage = document.getElementById('scoreMessage')
-const playerScorePara = document.getElementById('playerScore')
-const computerScorePara = document.getElementById('computerScore')
-const playerSign = document.getElementById('playerSign')
-const computerSign = document.getElementById('computerSign')
-const rockBtn = document.getElementById('rockBtn')
-const paperBtn = document.getElementById('paperBtn')
-const scissorsBtn = document.getElementById('scissorsBtn')
-const endgameModal = document.getElementById('endgameModal')
-const endgameMsg = document.getElementById('endgameMsg')
-const overlay = document.getElementById('overlay')
-const restartBtn = document.getElementById('restartBtn')
-
-rockBtn.addEventListener('click', () => handleClick('ROCK'))
-paperBtn.addEventListener('click', () => handleClick('PAPER'))
-scissorsBtn.addEventListener('click', () => handleClick('SCISSORS'))
-restartBtn.addEventListener('click', restartGame)
-overlay.addEventListener('click', closeEndgameModal)
 
 function handleClick(playerSelection) {
     if (isGameOver()) {
